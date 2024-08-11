65c9e970c07576b044429eb61e7eb737e2501647
sauravsable <sauravsable4102@gmail.com>
2024-08-11 17:56:53 +0530

    dev tool removed

--- src/store.js
+++ src/store.js
@@ -1,57 +1,76 @@
-import {createStore,combineReducers,applyMiddleware} from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import {thunk} from 'redux-thunk';
-import {composeWithDevTools} from 'redux-devtools-extension';
-import { newProductReducer, newReviewReducer, productDetailReducer, productReducer, productReviewsReducer, productsReducer, reviewReducer } from './reducers/productReducer';
-import { allUsersReducer, forgetpasswordReducer, profileReducer, userDetailsReducer, userReducer } from './reducers/userReducer';
-import { cartReducer } from './reducers/cartReducer';
-import { allOrderReducer, myOrderReducer, newOrderReducer, orderDetailReducer, orderReducer } from './reducers/orderReducer';
-import { newcartReducer } from './reducers/cartReducer';
-import { invitationReducer } from './reducers/cartReducer';
-import {addProductToCartReducer} from './reducers/cartReducer';
+import {
+  newProductReducer,
+  newReviewReducer,
+  productDetailReducer,
+  productReducer,
+  productReviewsReducer,
+  productsReducer,
+  reviewReducer
+} from './reducers/productReducer';
+import {
+  allUsersReducer,
+  forgetpasswordReducer,
+  profileReducer,
+  userDetailsReducer,
+  userReducer
+} from './reducers/userReducer';
+import {
+  cartReducer,
+  newcartReducer,
+  invitationReducer,
+  addProductToCartReducer
+} from './reducers/cartReducer';
+import {
+  allOrderReducer,
+  myOrderReducer,
+  newOrderReducer,
+  orderDetailReducer,
+  orderReducer
+} from './reducers/orderReducer';
 
 const reducer = combineReducers({
-    products : productsReducer,
-    productDetail :productDetailReducer,
-    user : userReducer,
-    profile : profileReducer,
-    forgotPassword : forgetpasswordReducer,
-    cart : cartReducer,
-    newOrder : newOrderReducer,
-    myOrders : myOrderReducer,
-    orderDetails : orderDetailReducer,
-    newReview : newReviewReducer,
-    newProduct : newProductReducer,
-    product : productReducer,
-    allOrders : allOrderReducer,
-    order : orderReducer,
-    allUsers: allUsersReducer,
-    userDetails: userDetailsReducer,
-    productReviews: productReviewsReducer,
-    review: reviewReducer,
-    newcart : newcartReducer,
-    invitation : invitationReducer,
-    cartProduct : addProductToCartReducer
+  products: productsReducer,
+  productDetail: productDetailReducer,
+  user: userReducer,
+  profile: profileReducer,
+  forgotPassword: forgetpasswordReducer,
+  cart: cartReducer,
+  newOrder: newOrderReducer,
+  myOrders: myOrderReducer,
+  orderDetails: orderDetailReducer,
+  newReview: newReviewReducer,
+  newProduct: newProductReducer,
+  product: productReducer,
+  allOrders: allOrderReducer,
+  order: orderReducer,
+  allUsers: allUsersReducer,
+  userDetails: userDetailsReducer,
+  productReviews: productReviewsReducer,
+  review: reviewReducer,
+  newcart: newcartReducer,
+  invitation: invitationReducer,
+  cartProduct: addProductToCartReducer
 });
 
-let initialState = {
-    cart : {
-        cartItems :localStorage.getItem("cartItems")
-        ? JSON.parse(localStorage.getItem("cartItems"))
-        : [],
-
-        shippingInfo : localStorage.getItem("shippingInfo")
-        ? JSON.parse(localStorage.getItem("shippingInfo"))
-        : {},
-    }
-
+const initialState = {
+  cart: {
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [],
+    shippingInfo: localStorage.getItem('shippingInfo')
+      ? JSON.parse(localStorage.getItem('shippingInfo'))
+      : {}
+  }
 };
 
 const middleware = [thunk];
 
 const store = createStore(
-    reducer,
-    initialState,
-    composeWithDevTools(applyMiddleware(...middleware))
+  reducer,
+  initialState,
+  applyMiddleware(...middleware)
 );
 
 export default store;
